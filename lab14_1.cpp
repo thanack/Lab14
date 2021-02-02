44e49795d878a62e97e1f729047261dc8d7e50b1
thanack <thanaphat2543@gmail.com>
2021-02-02 17:18:42 +0700

    14.1

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,7 +1,29 @@
-include <iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
+   int a = 5;
+   string b = "A";
+   string &c = b;
+   int *x = &a;
+   string *y = &b;
+   int *z = x;
+   
+   cout << a << " " << b << " " << c << " " << &x << " " << &y << " " << &z << endl;
+   cout << &a << " " << &b << " " << &c << " " << &x << " " << &y << " " << &z << endl;
+   
+   c = "F";
+   cout << a << " " << b << " " << c << " " << &x << " " << &y << " " << &z << endl;
+   
+   b = "W";
+   cout << a << " " << b << " " << c << " " << &x << " " << &y << " " << &z << endl;
+   
+   a = 6;
+   cout << a << " " << b << " " << c << " " << &x << " " << &y << " " << &z << endl;
+   
+   a = 7;
+   cout << a << " " << b << " " << c << " " << &x << " " << &y << " " << &z << endl;
 
 	
 	
