8c93775fb23601a9709ce14e7249867ba09f7860
thanack <thanaphat2543@gmail.com>
2021-02-02 17:19:24 +0700

    14.4

--- lab14_4.cpp
+++ lab14_4.cpp
@@ -17,3 +17,45 @@ int main(){
 	
 	return 0;
 }
+void swap(int &q ,int &w)
+{ 
+	int p = q;
+	q = w;
+	w = p ;
+}
+void shuffle(int &a,int &b,int &c,int &d)
+{
+	for(int s=0;s<5;s++)
+	{   int m;
+		m = rand()%4+1;
+	    switch(m)
+		{
+           case 1 : swap(a,b);
+		   break;
+		   case 2 : swap(a,c);
+		   break;
+		   case 3 : swap(a,d);
+		   break;
+		   case 4 : swap(b,a);
+		   break;
+		   case 5 : swap(b,c);
+		   break;
+		   case 6 : swap(b,d);
+		   break;
+		   case 7 : swap(c,a);
+		   break;
+		   case 8 : swap(c,b);
+		   break;
+		   case 9 : swap(c,d);
+		   break;
+		   case 10 : swap(d,a);
+		   break;
+		   case 11 : swap(d,b);
+		   break;
+		   case 12 : swap(d,c);
+		   break;
+		}
+
+	}
+	    
+}
